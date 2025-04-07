29187810d978357db7e80a013f446af1f033f4fb
Ashwinkarnati <ashwinkarnati2k5@gmail.com>
2025-04-07 19:22:04 +0530

    updated

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,6 +1,11 @@
 // app/create/page.jsx
+import { Suspense } from 'react'
 import CreateLogoClient from "./_components/CreateLogoClient"
 
 export default function CreatePage() {
-  return <CreateLogoClient />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CreateLogoClient />
+    </Suspense>
+  )
 }
